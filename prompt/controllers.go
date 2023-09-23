bce01d1c6a7556ccfb047436f6de34cb6d8c6f14
efen stakes <efenstakes101@gmail.com>
2023-09-23 22:50:51 +0300

    add index and prompt controller functions

---
+++ prompt/controllers.go
@@ -0,0 +1,59 @@
+package prompt
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/spf13/viper"
+)
+
+func Index(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"apiName": "Holmes AI",
+		"status":  "running",
+	})
+}
+
+func Prompt(c *gin.Context) {
+	apiKey, ok := viper.Get("OPENAPI_KEY").(string)
+
+	// get the prompt
+	prompt := c.DefaultQuery("prompt", "")
+
+	if prompt == "" || !ok {
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No prompt provided."})
+	}
+
+	client := openai.NewClient(apiKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+			MaxTokens: 800,
+		},
+	)
+
+	if err != nil {
+
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Prompt error. Holmes AI is out of tokens."})
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+
+	c.JSON(http.StatusOK, gin.H{
+		"prompt": prompt,
+		"reply":  resp.Choices[0].Message.Content,
+	})
+}
